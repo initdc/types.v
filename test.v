ff0b48459a259dae84ad3a2f38a7107e4945c60d
initdc <initd@outlook.com>
2025-12-29 01:13:24 +0800

    fix solution only working in single file

--- test.v
+++ test.v
@@ -1,5 +1,3 @@
-module option
-
 struct Opt[T] {
 	value T
 pub:
@@ -32,21 +30,6 @@ fn Opt.non[T]() Opt[T] {
 	}
 }
 
-fn (o Opt[T]) is_some() bool {
-	return o.some
-}
-
-fn (o Opt[T]) is_none() bool {
-	return !o.some
-}
-
-fn (o Opt[T]) is_some_and(f fn (T) bool) bool {
-	if !o.some {
-		return false
-	}
-	return f(o.value)
-}
-
 fn (o Opt[T]) map[U](f fn (T) U) Opt[U] {
 	if o.some {
 		v := f(o.value)
@@ -60,9 +43,10 @@ fn (o Opt[T]) map[U](f fn (T) U) Opt[U] {
 	}
 }
 
-fn (o Opt[T]) map_or[U](def U, f fn (T) U) U {
-	if o.some {
-		return f(o.value)
+fn main() {
+	a := Opt.some('hello')
+	map_len := fn (x string) int {
+		return x.len
 	}
-	return def
+	println(a.map(map_len))
 }
